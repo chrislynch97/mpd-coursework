0b575c1d7933e78b88237c2b4f37f80ca2936a90
Chris Lynch <christopher.lynch1997@gmail.com>
2019-04-07 19:06:05 +0100

    Tidied AboutFragment

--- app/src/main/java/org/clynch203/gcu/coursework/fragments/AboutFragment.java
+++ app/src/main/java/org/clynch203/gcu/coursework/fragments/AboutFragment.java
@@ -1,6 +1,13 @@
+//
+// Name                 Christopher Lynch
+// Student ID           S1511825
+// Programme of Study   Computing
+//
+
 package org.clynch203.gcu.coursework.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +17,7 @@ import org.clynch203.gcu.coursework.R;
 
 public class AboutFragment extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
