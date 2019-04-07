937ddb92ed3063e1b0c4e5c7b625d092f6bff985
Chris Lynch <christopher.lynch1997@gmail.com>
2019-04-07 17:57:14 +0100

    Tidied ItemActivity

--- app/src/main/java/org/clynch203/gcu/coursework/activities/ItemActivity.java
+++ app/src/main/java/org/clynch203/gcu/coursework/activities/ItemActivity.java
@@ -1,3 +1,9 @@
+//
+// Name                 Christopher Lynch
+// Student ID           S1511825
+// Programme of Study   Computing
+//
+
 package org.clynch203.gcu.coursework.activities;
 
 import android.content.Intent;
@@ -13,10 +19,14 @@ import android.widget.TextView;
 import org.clynch203.gcu.coursework.R;
 import org.clynch203.gcu.coursework.models.Item;
 
-public class ItemActivity extends AppCompatActivity {
+/**
+ * Activity for displaying all information on an Item.
+ */
+public class ItemActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Item item;
     private Button viewOnMapButton;
+    private FrameLayout backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,57 +35,58 @@ public class ItemActivity extends AppCompatActivity {
 
         ConstraintLayout layout = findViewById(R.id.item_activity);
 
+        Bundle bundle = getIntent().getExtras();
+        if (bundle == null) return; // TODO display error message
+
+        // sets the background color as the color passed in
+        // if no color is passed in then set as default color
         layout.setBackgroundColor(getIntent().getIntExtra("backgroundColor",
                 ResourcesCompat.getColor(getResources(), R.color.item_2_background, null)));
 
-        item = getIntent().getExtras().getParcelable("item");
+        item = bundle.getParcelable("item");
 
         displayItemData();
 
         viewOnMapButton = findViewById(R.id.viewOnMapButton);
-        viewOnMapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.putExtra("item", item);
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
+        viewOnMapButton.setOnClickListener(this);
 
-        FrameLayout backButton = findViewById(R.id.backButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(this);
     }
 
+    /**
+     * Displays all Item data in table.
+     */
     private void displayItemData() {
-        TextView location = findViewById(R.id.item_location);
-        location.setText(item.getLocation());
-
-        TextView magnitude = findViewById(R.id.item_magnitude);
-        magnitude.setText(String.valueOf(item.getMagnitude()));
-
-        TextView depth = findViewById(R.id.item_depth);
-        depth.setText(String.format(getResources().getString(R.string.depth_km), item.getDepth()));
-
-        TextView category = findViewById(R.id.item_category);
-        category.setText(item.getCategory());
-
-        TextView lat = findViewById(R.id.item_latitude);
-        lat.setText(String.valueOf(item.getLat()));
-
-        TextView lon = findViewById(R.id.item_longitude);
-        lon.setText(String.valueOf(item.getLon()));
-
-        TextView originDate = findViewById(R.id.item_origin_date);
-        originDate.setText(item.getOriginDateString());
-
-        TextView pubDate = findViewById(R.id.item_pub_date);
-        pubDate.setText(item.getPubDateString());
+        setViewText(R.id.item_location, item.getLocation());
+        setViewText(R.id.item_magnitude, String.valueOf(item.getMagnitude()));
+        setViewText(R.id.item_depth, String.format(getResources().getString(R.string.depth_km), item.getDepth()));
+        setViewText(R.id.item_category, item.getCategory());
+        setViewText(R.id.item_latitude, String.valueOf(item.getLat()));
+        setViewText(R.id.item_longitude, String.valueOf(item.getLon()));
+        setViewText(R.id.item_origin_date, item.getOriginDateString());
+        setViewText(R.id.item_pub_date, item.getPubDateString());
     }
 
+    /**
+     * Sets the text of given item id.
+     *
+     * @param id   ID of the view to set the text for.
+     * @param text Text to set.
+     */
+    private void setViewText(int id, String text) {
+        ((TextView) findViewById(id)).setText(text);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == viewOnMapButton) {
+            Intent intent = new Intent();
+            intent.putExtra("item", item);
+            setResult(RESULT_OK, intent);
+            finish();
+        } else if (v == backButton) {
+            finish();
+        }
+    }
 }
